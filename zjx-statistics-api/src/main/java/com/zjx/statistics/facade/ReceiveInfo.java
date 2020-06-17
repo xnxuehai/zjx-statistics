90f74a07c944df7197457d8ec2d795ef34a4ca94
xuxnuehai521@yeah.net <123456>
2020-06-17 09:16:55 +0800

    添加用户排行接口

--- zjx-statistics-api/src/main/java/com/zjx/statistics/facade/ReceiveInfo.java
+++ zjx-statistics-api/src/main/java/com/zjx/statistics/facade/ReceiveInfo.java
@@ -7,10 +7,20 @@ import com.zjx.statistics.dto.CounterDTO;
  * @date 2020/6/14 17:14
  */
 public interface ReceiveInfo {
-	/**
-	 * 接收统计数据
-	 *
-	 * @param counterDTO
-	 */
-	void receiveInfo(CounterDTO counterDTO);
+    /**
+     * 接收统计数据
+     *
+     * @param counterDTO
+     */
+    void receiveInfo(CounterDTO counterDTO);
+
+    /**
+     * 统计学生的经验值 和 奖章
+     *
+     * @param userId     userId
+     * @param experience experience
+     * @param medal      medal
+     */
+    void receiveInfo(Integer userId, Integer experience, Integer medal);
+
 }
